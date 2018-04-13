1946dc5931c670a2bb52c29ca49ca46fef689f31
Eduardo <euodeionomedeusuario@gmail.com>
2018-04-13 16:07:42 -0300

    adding Category.py

--- mamom/__init__.py
+++ mamom/__init__.py
@@ -16,3 +16,4 @@ from mamom.controllers import Users
 from mamom.models.User import User
 from mamom.models.Account import Account
 from mamom.models.Transation import Transation
+from mamom.models.Category import Category
