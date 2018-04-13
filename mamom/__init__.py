c8f5441f249c35cc16ca4987c7fac915e6080b69
Eduardo <euodeionomedeusuario@gmail.com>
2018-04-13 15:41:41 -0300

    adding run.py

---
+++ mamom/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "jkjaks7iwiou98w71w797"
+
+client = MongoClient("mongodb://localhost:27017/")
+
+db = client.mamomdb
