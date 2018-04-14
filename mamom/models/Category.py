4d24119bac83c0811b365ccd3aa4531fbd04bf35
Eduardo <euodeionomedeusuario@gmail.com>
2018-04-13 23:16:50 -0300

    implement getAllCategories method

--- mamom/models/Category.py
+++ mamom/models/Category.py
@@ -1,6 +1,14 @@
-
+from mamom import db
 
 class Category():
-    def __init__(self, id, name):
+    def __init__(self, id=0, name=""):
         self.id = id
         self.name = name
+
+    def getAllCategories(self):
+        try:
+            categories = db.categories.find({})
+
+            return categories
+        except Exception as e:
+            return None
