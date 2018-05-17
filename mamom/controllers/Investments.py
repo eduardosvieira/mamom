0b6bd92b7d9444831fc2ff06c60c2beff0d06a35
Eduardo <euodeionomedeusuario@gmail.com>
2018-05-17 09:38:31 -0300

    removing non use files

--- mamom/controllers/Investments.py
+++ mamom/controllers/Investments.py
@@ -2,24 +2,6 @@ from flask import render_template, request, jsonify
 
 from mamom import app
 
-from mamom.models.Savings import Savings
-from mamom.models.SELIC import SELIC
-
 @app.route("/mamom/investments/", methods=["GET"])
 def redirect_investments():
     return render_template("investments/investments.html")
-
-
-@app.route("/mamom/investments/", methods=["POST"])
-def invest():
-    value = float(request.form.get("value"))
-
-    investments = []
-
-    savings = Savings(value=value, name="Cardeneta de Poupança")
-    selic = SELIC(value=value, name="Tesouro SELIC")
-
-    investments.append({"name": savings.name, "value": savings.calculate() })
-    investments.append({"name": selic.name, "value": selic.calculate() })
-
-    return jsonify(investments)
--- mamom/models/Investment.py
+++
@@ -1,8 +0,0 @@
-
-class Investment():
-    def __init__(self, value=0, name=""):
-        self.value = value,
-        self.name = name
-
-    def calcule(self):
-        pass
