e67a882b6ca7c7a381c9045a6a49253be2a743a0
Eduardo <euodeionomedeusuario@gmail.com>
2018-05-17 09:52:13 -0300

    blocking access to simulation investments page

--- mamom/controllers/Investments.py
+++ mamom/controllers/Investments.py
@@ -1,7 +1,10 @@
-from flask import render_template, request, jsonify
+from flask import render_template, request, jsonify, session, redirect
 
 from mamom import app
 
 @app.route("/mamom/investments/", methods=["GET"])
 def redirect_investments():
-    return render_template("investments/investments.html")
+    if "_id" in session:
+        return render_template("investments/investments.html")
+
+    return redirect("/mamom/login/")
