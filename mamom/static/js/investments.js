4cedbe1c9a0cf34f8773667341387e00954e4dd0
Eduardo <euodeionomedeusuario@gmail.com>
2018-05-20 19:22:48 -0300

    implementing view debts

--- mamom/static/js/investments.js
+++ mamom/static/js/investments.js
@@ -8,41 +8,58 @@ var URL = PROTOCOL + "//" + HOSTNAME + ":" + PORT;
 
 $(document).ready(function(event) {
   var savings = {
-    title: "Cardeneta de Poupança",
+    title: "Cardeneta de Poupança (6,00%)",
     juros: 0.005,
+    imposto: 0,
 
     calcule: function(value, months) {
       return value * Math.pow(1 + this.juros, months);
+    },
+
+    calculeImpostos: function(value) {
+      return value * this.imposto;
     }
   };
 
   var selic = {
-    title: "Tesouro SELIC",
+    title: "Tesouro SELIC (6,50%)",
     juros: 0.00542,
+    imposto: 0.22,
 
     calcule: function(value, months) {
       return value * Math.pow(1 + this.juros, months);
+    },
+
+    calculeImpostos: function(value) {
+      return value * this.imposto;
     }
   };
 
   var ipca = {
-    title: "Tesouro IPCA+",
+    title: "Tesouro IPCA+ (4,50% + 3,0%)",
     juros: 0.0075,
+    imposto: 0.22,
 
     calcule: function(value, months) {
       return value * Math.pow(1 + this.juros, months);
+    },
+
+    calculeImpostos: function(value) {
+      return value * this.imposto;
     }
+
   };
 
   $("#value").change(function(event) {
     var value = $(this).val();
 
     investments = [savings, selic, ipca];
-    console.log(investments);
 
     $("#investments").empty();
 
     for(index in investments) {
+      var lucro = Math.trunc(investments[index].calcule(value, 6));
+
       var $investment = $("<div />")
                           .addClass("card")
                           .append($("<div />")
@@ -53,19 +70,26 @@ $(document).ready(function(event) {
                             .append($("<div />")
                               .addClass("row")
                               .append($("<div />")
-                                .addClass("col s12 m6 l6")
+                                .addClass("col s12 m4 l4")
                                 .append($("<label />")
                                   .text("Valor Inicial"))
                                 .append($("<span />")
                                   .addClass("card-title")
                                   .text(Math.trunc(value))))
                               .append($("<div />")
-                                .addClass("col s12 m6 l6")
+                                .addClass("col s12 m4 l4")
                                 .append($("<label />")
                                   .text("Valor após 6 meses"))
                                 .append($("<span />")
                                   .addClass("card-title")
-                                  .text(Math.trunc(investments[index].calcule(value, 6)))))));
+                                  .text(lucro)))
+                                .append($("<div />")
+                                  .addClass("col s12 m4 l4")
+                                  .append($("<label />")
+                                    .text("Impostos"))
+                                  .append($("<span />")
+                                    .addClass("card-title")
+                                    .text(investments[index].calculeImpostos(lucro - value))))));
 
       $("#investments").append($investment);
     }
